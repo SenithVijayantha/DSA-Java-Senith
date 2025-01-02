add6b8d48ceb68694ef9b21d67e3b8e231d73f30
SenithSV <ssvsenith@gmail.com>
2025-01-02 22:41:05 +0530

    Add some code into LargeNum.java

--- Conditionals-and-loops/LargeNum.java
+++ Conditionals-and-loops/LargeNum.java
@@ -1,3 +1,29 @@
+import java.util.Scanner;
+
 public class LargeNum {
-    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+
+//        Q: Find the largest of the 3 numbers
+
+//        Using if
+        int max = a;
+
+        if (b > max) {
+            max = b;
+        } if (c > max) {
+            max = c;
+        }
+
+        System.out.println(max);
+
+//        Using Math.max
+
+        max = Math.max(c, Math.max(a,b));
+        System.out.println(max);
+    }
 }
