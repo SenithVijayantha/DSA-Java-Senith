c01afac93bd0ec1861cfee60105a00c9f7dd599c
SenithSV <ssvsenith@gmail.com>
2025-01-03 19:12:36 +0530

    Add example code for switch cases

--- switch/switch/src/Main.java
+++ switch/switch/src/Main.java
@@ -1,5 +1,44 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a name of a fruit: ");
+        String fruit = in.next();
+
+        switch (fruit) {
+            case "Mango":
+                System.out.println("King of fruits");
+                break;
+            case "Apple":
+                System.out.println("A sweet red fruit");
+                break;
+            case "Orange":
+                System.out.println("Round fruit");
+            case "Grapes":
+                System.out.println("Small fruit");
+            default:
+                System.out.println("please enter a valid fruit");
+        }
+
+        int day = in.nextInt();
+
+//        Enhanced switch case
+        switch (day) {
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            default -> System.out.println("Enter a number between 1 and 5");
+        }
+
+        day = in.nextInt();
+
+        switch (day) {
+            case 1,2,3,4,5 -> System.out.println("Weekday");
+            case 6,7 -> System.out.println("Weekend");
+        }
+
     }
 }
