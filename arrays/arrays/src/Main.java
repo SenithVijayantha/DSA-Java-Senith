33a6daeb79b0652bd3fd22dee6b0e3e10899c1f8
SenithSV <ssvsenith@gmail.com>
2025-01-04 19:26:38 +0530

    Add code in array.java

--- arrays/arrays/src/Main.java
+++ arrays/arrays/src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner in = new Scanner(System.in);
+
+        int[][] arr = new int[3][2];
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < 2; j++) {
+                arr[i][j] = in.nextInt();
+            }
+            System.out.println(Arrays.toString(arr[i]));
+        }
+
+
     }
 }
