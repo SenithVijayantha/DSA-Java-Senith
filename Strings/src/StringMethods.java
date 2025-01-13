919ad31c05b6e14a4dcb97ce90937a990a3f56eb
SenithSV <ssvsenith@gmail.com>
2025-01-13 10:07:08 +0530

    Play with some methods of the String class

--- Strings/src/StringMethods.java
+++ Strings/src/StringMethods.java
@@ -1,3 +1,13 @@
+import java.util.Arrays;
+import java.util.Locale;
+
 public class StringMethods {
-    
+    public static void main(String[] args) {
+        String name = "Senith";
+        System.out.println(Arrays.toString(name.toCharArray()));
+        System.out.println(name.toLowerCase());
+        System.out.println(name.toUpperCase());
+        System.out.println(name.indexOf('e'));
+        
+    }
 }
