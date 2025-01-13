9d3b6a2a857795e9034f442937f8193c92ff1b36
SenithSV <ssvsenith@gmail.com>
2025-01-13 09:32:15 +0530

    Write code to format strings

--- Strings/src/PrettyPrinting.java
+++ Strings/src/PrettyPrinting.java
@@ -1,3 +1,12 @@
 public class PrettyPrinting {
-    
+    public static void main(String[] args) {
+        float a = 2342.1213f;
+        System.out.printf("Formatted number is %.2f", a);
+        System.out.println();
+        System.out.printf("Pie: %.3f", Math.PI);
+        System.out.println();
+        System.out.printf("Hello my name is %s and I'm %s", "Senith", "Cool");
+    }
+
+
 }
